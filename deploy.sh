2f67f43a5727bf966a2553b9ebfaeaf0807389a3
Yoshimura Yuu <yyu@mental.poker>
2017-12-17 01:05:15 +0900

    Change deploy OS to linux from macOS

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-if [[ "${TRAVIS_OS_NAME}" == "osx" && "${TRAVIS_BRANCH}" == "master" && "${TRAVIS_PULL_REQUEST}" == "false" ]]; then
+if [[ "${TRAVIS_OS_NAME}" == "linux" && "${TRAVIS_BRANCH}" == "master" && "${TRAVIS_PULL_REQUEST}" == "false" ]]; then
   echo -e "Host github.com\n\tStrictHostKeyChecking no\nIdentityFile ~/.ssh/deploy.key\n" >> ~/.ssh/config
   openssl aes-256-cbc -k "$SERVER_KEY" -in deploy_key.enc -d -a -out deploy.key
   cp deploy.key ~/.ssh/
